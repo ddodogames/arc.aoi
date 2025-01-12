98a70aef08ac3d202fbd5bdf429808db1e4a454c
ddodogames <69091682+ddodogames@users.noreply.github.com>
2025-01-12 19:38:40 +0200

    match the names

--- arc.aoi/arcfunctions.js
+++ arc.aoi/arcfunctions.js
@@ -1,11 +1,11 @@
-class Plugins {
+class ArcFunctions {
   constructor(args) {
     this.args = args;
     if (!args.client) {
       console.log("You have not specified your aoi client's name! Exiting Code!");
       process.exit(0);
     }
-  } loadPlugins() {
+  } loadArcFunctions() {
     const client = this.args.client;
 
     client.functionManager.createFunction({
