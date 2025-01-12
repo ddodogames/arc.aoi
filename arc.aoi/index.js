f3117d027b76227ba37000e51fd3d84623ba6525
dodoGames <69091682+ddodogames@users.noreply.github.com>
2025-01-12 20:34:57 +0200

    sync

--- arc.aoi/index.js
+++ arc.aoi/index.js
@@ -1,5 +1,10 @@
 const { ArcFunctions } = require("./arcfunctions.js");
+const chalk = require("chalk")
+const { version } = require('../package.json');
 
 module.exports = {
   ArcFunctions
 };
+
+console.log(chalk.red(`Modified Version of aoi.js is installed. Please do not install official and use modified one because its meant to be used for arc.aoi`))
+console.log(chalk.green(`arc.aoi - Version: ${version}`));
